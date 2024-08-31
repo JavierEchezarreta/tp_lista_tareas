6e075ef87a4f62ecd79410d99099a38ec344368e
JavierEchezarreta <javier.echezarreta@alumnos.ispi4038.edu.ar>
2024-08-31 18:14:10 -0300

    Desarrollo de los tests de las funcionalidades previstas

---
+++ tests/test_gestor_de_tareas.py
@@ -0,0 +1,37 @@
+import pytest
+from src.gestor_de_tareas import GestorDeTareas
+
+@pytest.fixture
+def gestor():
+    return GestorDeTareas()
+
+def test_agregar_tarea(gestor):
+
+    gestor.agregar_tarea("Tarea de prueba")
+    assert gestor.buscar_tarea("Tarea de prueba").descripcion_coincide("Tarea de prueba")
+
+    gestor.agregar_tarea("Segunda tarea de prueba")
+    assert gestor.buscar_tarea("Segunda tarea de prueba").descripcion_coincide("Segunda tarea de prueba")
+
+def test_completar_tarea(gestor):
+
+    gestor.agregar_tarea("Tarea de prueba")
+    gestor.completar_tarea("Tarea de prueba")
+    assert gestor.tarea_completada("Tarea de prueba") == True
+
+    gestor.agregar_tarea("Segunda tarea de prueba")
+    gestor.completar_tarea("Segunda tarea de prueba")
+    assert gestor.tarea_completada("Segunda tarea de prueba") == True
+
+def test_eliminar_tarea(gestor):
+
+    gestor.agregar_tarea("Tarea de prueba")
+    gestor.eliminar_tarea("Tarea de prueba")
+    with pytest.raises(ValueError):
+        gestor.buscar_tarea("Tarea de prueba")
+
+    gestor.agregar_tarea("Segunda tarea de prueba")
+    gestor.eliminar_tarea("Segunda tarea de prueba")
+    with pytest.raises(ValueError):
+        gestor.buscar_tarea("Segunda tarea de prueba")
+
